8baeba0d35a705d97fd6ece890999fe8df5833c9
Michael <michaelksullivan50@gmail.com>
2024-04-18 17:39:03 +0200

    Change port var

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -4,7 +4,7 @@
 module.exports = ({ env }) => ({
   proxy: true,
   host: "0.0.0.0",
-  port: process.env.PORT,
+  port: env("PORT"),
   url: env("MY_HEROKU_URL"),
   app: {
     keys: env.array("APP_KEYS"),
